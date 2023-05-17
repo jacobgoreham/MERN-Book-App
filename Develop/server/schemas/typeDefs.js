58aa5a31cd61a494f514a834b9078221dc4f390e
Jacob Goreham <jacobgoreham@gmail.com>
2023-05-16 22:58:27 -0700

    finished typeDefs and running, upload to heroku next

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -12,6 +12,34 @@ const typeDefs = gql`
     title: String
   }
   type User {
-    
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Query {
+    me: User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input SavedBookInput {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    forSale: String
+    link: String
+    title: String
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SaveBookInput): User
+    removeBoook(bookId: String!): User
   }
 `;
+
+module.exports = typeDefs;
